7838be619a462bb38b70a7c5db314c7f6db0f651
b23production GNU <b23prodtm@users.sourceforge.net>
2019-04-05 22:08:05 +0200

    rapsi A : mem config

--- scripts/my_cluster.sh
+++ scripts/my_cluster.sh
@@ -28,8 +28,9 @@ if [[ "$#" -gt 1 ]]; then IPS=$@; else while [[ -z $IPS ]]; do
       echo -e "Enter two or more valid IP addresses of the form X.X.X.X : X€[0;255] \n"
   fi
 done; fi 
-logger -t kubespray "IPS=(${IPS[@]})\n"
+logger -st kubespray "IPS=(${IPS[@]})\n"
 YAML=inventory/mycluster/hosts.yaml
+INI=inventory/mycluster/hosts.ini
 HOSTS="-95.54.0.21 -95.54.0.22"
 python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg
 [ $(cfrm_act "the machines are up and running" 'y') > /dev/null ] || exit 0
@@ -41,22 +42,23 @@ cat inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
 	
 declare PI=ubuntu # replace 'pi' with 'ubuntu' or any other user
 for ip in ${IPS[@]}; do
-  logger -t kubespray-$ip "****** K8s ip : $PI@$ip ******"
+  logger -st kubespray-$ip "****** K8s ip : $PI@$ip ******"
   ssh-copy-id $PI@$ip;    
-  logger -t kubespray-$ip "configure sshd"
-  ssh $PI@$ip sudo bash -c "echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config";
+  logger -st kubespray-$ip "configure sshd"
+  ssh $PI@$ip "sudo echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config";
   ssh $PI@$ip sudo cat /etc/ssh/sshd_config | grep PermitRootLogin;
-  logger -t kubespray-$ip "install go"
+  logger -st kubespray-$ip "install go"
   ssh $PI@$ip sudo apt-get install golang -y;
-  ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367;
+  #ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 &
   ssh $PI@$pi sudo usermod -a -G ubuntu kube;
-  logger -t kubespray-$ip "disable firewall"
+  logger -st kubespray-$ip "disable firewall"
   ssh $PI@$pi sudo ufw disable;
 done
 cat roles/kubernetes/preinstall/tasks/0020-verify-settings.yml | grep -b2 'that: ansible_memtotal_mb'
-scripts/my_playbook.sh -i $YAML cluster.yml --timeout=60
+scripts/my_playbook.sh -i $INI cluster.yml --timeout=60
+#scripts/my_playbook.sh -i $YAML cluster.yml --timeout=60
 for ip in ${IPS[@]}; do
    scripts/my_playbook.sh --setup-firewall $PI@$pi
-   logger -t kubespray-$ip "enable firewall"
+   logger -st kubespray-$ip "enable firewall"
    ssh $PI@$pi sudo ufw enable;        
 done
