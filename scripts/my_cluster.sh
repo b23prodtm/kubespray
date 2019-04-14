08c712a0a0cdf079e0d36fd191b00c52fc23129c
b23production GNU <b23prodtm@users.sourceforge.net>
2019-04-14 13:16:37 +0200

    Quick start scripts Guidelines

--- scripts/my_cluster.sh
+++ scripts/my_cluster.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-#
-# Update Ansible inventory file with inventory builder . Single master IP is possible, see nodes with bastion
+# Usage: $ scripts/my_cluster.sh
+# Update Ansible inventory file with inventory builder . Single master cluster is possible.
+# How to use 2 nodes with Calico and Route Reflectors, in docs/calico.md.
 #
 function cfrm_act () {
 def_go=$2
@@ -32,9 +33,10 @@ logger -st kubespray "IPS=(${IPS[@]})\n"
 YAML=inventory/mycluster/hosts.yaml
 INI=inventory/mycluster/hosts.ini
 HOSTS="-95.54.0.21 -95.54.0.22"
+logger -st kubespray "****** K8s ansible : Generate $INI and $YAML ******"
 python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg
 [ $(cfrm_act "the machines are up and running" 'y') > /dev/null ] || exit 0
-python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg > $YAML
+CONFIG_FILE=$INI python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg > $YAML
 cat inventory/mycluster/group_vars/all/all.yml
 [ $(cfrm_act "the options" 'y') > /dev/null ] || exit 0
 cat inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
@@ -49,16 +51,27 @@ for ip in ${IPS[@]}; do
   ssh $PI@$ip sudo cat /etc/ssh/sshd_config | grep PermitRootLogin;
   logger -st kubespray-$ip "install go"
   ssh $PI@$ip sudo apt-get install golang -y;
-  #ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 &
+  logger -st kubespray-$ip "trusted repository"
+  ssh $PI@$ip echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+  ssh $PI@$ip echo "deb http://ppa.launchpad.net/projectatomic/ppa/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+  ssh $PI@$ip echo "deb http://ppa.launchpad.net/alexlarsson/flatpak/ubuntu bionic main" | sudo tee -a /etc/apt/sources.list
+  logger -st kubespray-$ip "add kube user into ubuntu group"
   ssh $PI@$ip sudo usermod -a -G ubuntu kube;
-  logger -st kubespray-$ip "disable firewall"
+  logger -st kubespray-$ip "disable ubuntu firewall"
   ssh $PI@$ip sudo ufw disable;
+  # logger -st kubespray-$ip "Launchpad PPA repository keys"
+  # ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367 &
+  # ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8becf1637ad8c79d &
+  # ssh $PI@$ip sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys c793bfa2fa577f07 &
 done
 cat roles/kubernetes/preinstall/tasks/0020-verify-settings.yml | grep -b2 'that: ansible_memtotal_mb'
+logger -st kubespray "****** K8s ansible : Run Playbook cluster.yml ******"
 scripts/my_playbook.sh -i $INI cluster.yml --timeout=60
 #scripts/my_playbook.sh -i $YAML cluster.yml --timeout=60
 for ip in ${IPS[@]}; do
-   scripts/my_playbook.sh --firewall-setup $PI@$ip status
+   logger -st kubespray-$ip "****** K8s ip : $PI@$ip ******"
    logger -st kubespray-$ip "enable firewall"
-   ssh $PI@$ip sudo ufw enable;
+   scripts/my_playbook.sh --firewall-setup $PI@$ip enable
+   scripts/my_playbook.sh --firewall-setup $PI@$ip status
+
 done
--- scripts/my_playbook.sh
+++ scripts/my_playbook.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
 function setup_crio() {
+   logger -st kubespray "CRI-O's currently unstable in Kubespray"
    ssh $1 '
    sudo add-apt-repository ppa:alexlarsson/flatpak;
    sudo add-apt-repository ppa:projectatomic/ppa;
@@ -12,7 +14,7 @@ function setup_firewall() {
   source my_firewall.sh $*
 }
 inventory='inventory/mycluster'
-defaults='-b --private-key=~/.ssh/id_rsa --ask-become-pass'
+defaults='-b --private-key=~/.ssh/id_rsa'
 options=""
 usage="Usage: $0 [-i,--inventory <inventory/path/to/hosts.ini>] <yaml> [ansible-playbook options]"
 usage2="Usage: $0 --crio-setup|--firewall-setup <user>@<master-node-ip> status|enable|disable|..."
@@ -44,5 +46,4 @@ logger -s "Disable the cluster firewall if you can..."
 logger -s "If a TASK failed on timeouts, reboot the kubernetes cluster before to retry..."
 logger -s "Known TASKs that take more time : [Start of Calico kube controllers], [Kubernetes Apps | Start Resources]..."
 logger -s "It's going to take about half an hour per host to complete the cluster boot process..."
-logger -s "Please don't shutdown anything until it finishes..." && ansible-playbook -i ${inventory}/hosts.ini $defaults $options && echo "Next call must be ${inventory}/artifacts/kubectl.sh :
-${usage2}"
+logger -s "Please don't shutdown anything until it finishes..." && ansible-playbook -i ${inventory}/hosts.ini $defaults $options && echo "Next call must be scripts/start_dashboard.sh --timeout=60"
