84dde235d0212c1b90124ed867b85301b35950bc
b23production GNU <b23prodtm@users.sourceforge.net>
2019-04-14 20:36:53 +0200

    Update my_cluster.sh

--- scripts/my_cluster.sh
+++ scripts/my_cluster.sh
@@ -22,9 +22,9 @@ if [[ "$#" -gt 1 ]]; then IPS=$@; else while [[ -z $IPS ]]; do
 	read -p "Please type in up to 6 local network ip${IFS}2°ip${IFS}3°ip...: (CTRL-C to exit) " -a ips
 	echo -e "\n"
   if [[ ${#ips[@]} -gt 1 ]]; then
-	if [[ $(cfrm_act "you've entered the correct ips addresses ${ips[0]} ${ips[1]} ${ips[2]} ${ips[3]} ${ips[4]} ${ips[5]}" 'n') > /dev/null ]]; then
-		IPS=${ips[@]}
-	fi
+  	if [[ $(cfrm_act "you've entered the correct ips addresses ${ips[0]} ${ips[1]} ${ips[2]} ${ips[3]} ${ips[4]} ${ips[5]}" 'n') > /dev/null ]]; then
+  		IPS=${ips[@]}
+  	fi
   else
       echo -e "Enter two or more valid IP addresses of the form X.X.X.X : X€[0;255] \n"
   fi
@@ -32,11 +32,11 @@ done; fi
 logger -st kubespray "IPS=(${IPS[@]})\n"
 YAML=inventory/mycluster/hosts.yaml
 INI=inventory/mycluster/hosts.ini
-HOSTS="-95.54.0.21 -95.54.0.22"
 logger -st kubespray "****** K8s ansible : Generate $INI and $YAML ******"
-python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg
-[ $(cfrm_act "the machines are up and running" 'y') > /dev/null ] || exit 0
-CONFIG_FILE=$INI python contrib/inventory_builder/inventory.py $HOSTS ${IPS[@]} print_cfg > $YAML
+python contrib/inventory_builder/inventory.py ${IPS[@]} print_cfg
+if [ $(cfrm_act "Regenerate the $INI file, are the machines up and running" 'n') > /dev/null ]; then
+  CONFIG_FILE=$INI python contrib/inventory_builder/inventory.py ${IPS[@]} print_cfg > $YAML
+fi
 cat inventory/mycluster/group_vars/all/all.yml
 [ $(cfrm_act "the options" 'y') > /dev/null ] || exit 0
 cat inventory/mycluster/group_vars/k8s-cluster/k8s-cluster.yml
